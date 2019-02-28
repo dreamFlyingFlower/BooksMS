dea7ef00107015183f5820bef5803e282dd71a6a
ParadiseWY <582822832@qq.com>
2019-02-28 10:56:17 +0800

    fixfastjsonconfig

--- bms-common/src/main/java/com/wy/config/JsonConfig.java
+++ bms-common/src/main/java/com/wy/config/JsonConfig.java
@@ -17,7 +17,6 @@ import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 
 /**
  * 替换springboot默认的jackjson为fastjson
- * 
  * @author paradiseWy
  */
 @Configuration
@@ -29,10 +28,26 @@ public class JsonConfig {
 	public HttpMessageConverters fastJsonConfig(JacksonProperties properties) {
 		FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
 		FastJsonConfig fastJsonConfig = new FastJsonConfig();
-		fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
+		fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat,
+				SerializerFeature.WriteNullStringAsEmpty, SerializerFeature.WriteNullListAsEmpty,
+				SerializerFeature.WriteMapNullValue,
+				SerializerFeature.DisableCircularReferenceDetect);
+		/**--- 若数据库字段全为大写,且使用了fastjson代替jackjson,可使用如下配置 ---**/
+//		SerializeConfig parser = new SerializeConfig();
+//		// 解决swagger2在将字段都变成大写之后无法访问的问题
+//		NameFilter nameFilter = (object,name,value)->name;
+//		parser.addFilter(UiConfiguration.class, nameFilter);
+//		parser.addFilter(SwaggerResource.class, nameFilter);
+//		// 若数据库字段全为大写开头,此处配置改为大写
+//		// 必须放在filter之后,否则无法访问swagger2
+//		parser.propertyNamingStrategy = PropertyNamingStrategy.PascalCase;
+//		fastJsonConfig.setSerializeConfig(parser);
+		/**------------------------------------------------------------------------------**/
 		// 处理中文乱码问题
 		List<MediaType> fastMediaTypes = new ArrayList<>();
 		fastMediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
+		fastMediaTypes.add(MediaType.APPLICATION_FORM_URLENCODED);
+		fastMediaTypes.add(MediaType.APPLICATION_OCTET_STREAM);
 		fastConverter.setSupportedMediaTypes(fastMediaTypes);
 		// 日期格式化
 		fastJsonConfig.setDateFormat(properties.getDateFormat());
